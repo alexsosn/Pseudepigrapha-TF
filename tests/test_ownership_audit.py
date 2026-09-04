36726fb5f849c7defabf77d2deab5822f42e2d70
Oleksandr (Alex) Sosnovshchenko <alex.sosn88@gmail.com>
2026-09-05 02:16:05 +0300

    test: cover ownership cardinality and target types

--- tests/test_ownership_audit.py
+++ tests/test_ownership_audit.py
@@ -28,6 +28,16 @@ def _node(data, kind, feature, value):
     )
 
 
+def _node_in_version(data, kind, feature, value, version_id):
+    return next(
+        node
+        for node, node_kind in data.node_features["otype"].items()
+        if node_kind == kind
+        and data.node_features.get(feature, {}).get(node) == value
+        and data.node_features.get("version_id", {}).get(node) == version_id
+    )
+
+
 def _owner(data, version_id):
     textual = data.node_features.get("book", {})
     for node, value in textual.items():
@@ -60,6 +70,27 @@ def test_audit_rejects_reading_ownership_corruption(tmp_path, mutation):
     _assert_only_ownership_failed(report, "reading_ownership")
 
 
+def test_audit_rejects_reading_with_second_owner(tmp_path):
+    books, data = _case(tmp_path)
+    alpha = _node(data, "reading", "reading_text", "alpha")
+    beta = _node(data, "reading", "reading_text", "beta")
+    beta_unit = next(iter(data.edge_features["reading_of"][beta]))
+    data.edge_features["reading_of"][alpha].add(beta_unit)
+
+    report = build_conversion_report(tmp_path, books, data)
+    _assert_only_ownership_failed(report, "reading_ownership")
+
+
+def test_audit_rejects_reading_owner_of_wrong_type(tmp_path):
+    books, data = _case(tmp_path)
+    alpha = _node(data, "reading", "reading_text", "alpha")
+    same_locus_div = _node_in_version(data, "div", "source_ref", "1:1", "Ownership__Greek")
+    data.edge_features["reading_of"][alpha] = {same_locus_div}
+
+    report = build_conversion_report(tmp_path, books, data)
+    _assert_only_ownership_failed(report, "reading_ownership")
+
+
 @pytest.mark.parametrize(
     ("abbrev", "mutation", "wrong_owner"),
     [
@@ -83,6 +114,27 @@ def test_audit_rejects_manuscript_ownership_corruption(tmp_path, abbrev, mutatio
     _assert_only_ownership_failed(report, "manuscript_ownership")
 
 
+def test_audit_rejects_manuscript_with_second_owner(tmp_path):
+    books, data = _case(tmp_path)
+    manuscript = _node(data, "manuscript", "ms_abbrev", "A")
+    data.edge_features["manuscript_of"][manuscript].add(_owner(data, "Ownership__Greek_2"))
+
+    report = build_conversion_report(tmp_path, books, data)
+    _assert_only_ownership_failed(report, "manuscript_ownership")
+
+
+def test_audit_rejects_manuscript_owner_of_wrong_type(tmp_path):
+    books, data = _case(tmp_path)
+    manuscript = _node(data, "manuscript", "ms_abbrev", "A")
+    same_version_resource = _node_in_version(
+        data, "resource", "resource_name", "Edition A", "Ownership__Greek"
+    )
+    data.edge_features["manuscript_of"][manuscript] = {same_version_resource}
+
+    report = build_conversion_report(tmp_path, books, data)
+    _assert_only_ownership_failed(report, "manuscript_ownership")
+
+
 @pytest.mark.parametrize(
     ("resource_name", "mutation", "wrong_owner"),
     [
@@ -100,6 +152,27 @@ def test_audit_rejects_resource_ownership_corruption(tmp_path, resource_name, mu
         data.edge_features["resource_of"].pop(resource)
     else:
         data.edge_features["resource_of"][resource] = {_owner(data, wrong_owner)}
+
+    report = build_conversion_report(tmp_path, books, data)
+    _assert_only_ownership_failed(report, "resource_ownership")
+
+
+def test_audit_rejects_resource_with_second_owner(tmp_path):
+    books, data = _case(tmp_path)
+    resource = _node(data, "resource", "resource_name", "Edition A")
+    data.edge_features["resource_of"][resource].add(_owner(data, "Ownership__Greek_2"))
+
+    report = build_conversion_report(tmp_path, books, data)
+    _assert_only_ownership_failed(report, "resource_ownership")
+
+
+def test_audit_rejects_resource_owner_of_wrong_type(tmp_path):
+    books, data = _case(tmp_path)
+    resource = _node(data, "resource", "resource_name", "Edition A")
+    same_version_manuscript = _node_in_version(
+        data, "manuscript", "ms_abbrev", "A", "Ownership__Greek"
+    )
+    data.edge_features["resource_of"][resource] = {same_version_manuscript}
 
     report = build_conversion_report(tmp_path, books, data)
     _assert_only_ownership_failed(report, "resource_ownership")
